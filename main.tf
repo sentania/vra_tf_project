50ad92ef4ef48049a4138727ea37ef7041c931b3
Scott Bowe <scottb@sentania.net>
2020-06-24 15:12:06 -0500

    Upload for Jenkins

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "vra" {
   refresh_token = var.refresh_token
   insecure      = var.insecure
 }
-
 data "vra_zone" "this" {
-  name = var.zone_name
-}
+      name = var.zone_name
+}  
 
 resource "vra_project" "this" {
+
   name        = var.project_name
   description = "terraform test project"
 
@@ -16,9 +16,9 @@ resource "vra_project" "this" {
     zone_id          = data.vra_zone.this.id
     priority         = 1
     max_instances    = 2
-    cpu_limit        = 1024
-    memory_limit_mb  = 8192
-    storage_limit_gb = 65536
+    cpu_limit        = 128
+    memory_limit_mb  = 256
+    storage_limit_gb = 8192
   }
 
   shared_resources = true
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,5 @@
 url = "https://vra8.lab.sentania.net/"
 zone_name = "WLD 1 / lab-vcf-w01-DC"
 project_name = "Test Project 2"
+insecure = false
 
